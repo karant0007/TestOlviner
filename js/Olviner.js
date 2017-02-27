f517b7cd80c5985a185802baf818bc4f5696fcd7
karan <karant0007@gmail.com>
2017-02-28 01:12:54 +0530

    new final updates dynamic

--- js/Olviner.js
+++ js/Olviner.js
@@ -1,53 +1,53 @@
 ﻿$rm_CommingSoon = $('#rm_CommingSoon');
 $rm_DataSection = $('#rm_DataSection');
+$NewsBody = $('#NewsBody');
 $(document).ready(function () {
-    debugger;
     BindIntents();
+    $('#NewsModal').modal('show');
 });
 function BindIntents() {
     $.ajax({
-        url: 'http://demolviner.somee.com/WebService/OlvinerServices.svc/Web/Content',
+        type: "POST",
+        url: "http://localhost:53111/WebService/OlvinerServices.svc/Web/Content",
+        data: '{}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
         success: function (response) {
-            alert('Success');
+            var result = response.Result;
+            var resultContent = result.Content;
+            var NewsData = resultContent[0];
+            var rmData = resultContent[1];
+
+            BindNewsContents(NewsData);
+            BindReasarchMathods(rmData.MediaContent);
         },
         error: function (response) {
-            alert('Failed');
+            //alert('Failed');
         }
+
     });
-    //$.ajax({
-    //    type: "POST",
-    //    crossDomain: "true",
-    //    url: "http://demolviner.somee.com/WebService/OlvinerServices.svc/Web/Content",
-    //    data: "{}",
-    //    contentType: "application/json; charset=utf-8",
-    //    dataType: "json",
-    //    headers: {
-    //        "cache-control": "no-cache",
-    //        "postman-token": "ce957fd9-64a6-67ad-3cef-a1130911eed0"
-    //    },
-    //    success: function (response) {
-    //        alert('Success');
-    //    },
-    //    error: function (response) {
-    //        alert('Failed');
-    //    }
-
-    //});
 };
-
+function BindNewsContents(data) {
+    try {
+        if (data != undefined) {
+            $NewsBody.empty();
+            $NewsBody.html(data.TextContent);
+        }
+    } catch (e) {}
+};
 function BindReasarchMathods(data) {
     try {
         $rm_DataSection.empty();
-        if (data == undefined) {
+        if (data != undefined) {
             $rm_CommingSoon.hide();
             $rm_DataSection.show();
 
             $rm_DataSection.owlCarousel();
             var dataCarosello = $rm_DataSection.data('owlCarousel');
-            for (var i = 0; i < 20; i++) {
+            for (var i = 0; i < data.length; i++) {
                 var htmlStr = "<div class='item text-center'>" +
-                                    "<div class='rm'> <a> <img src='img/Coupans/1.png'/></a> </div>" +
-                                    "<p>ABCD</p>" +
+                                    "<div class='rm'> <a> <img src='" + data[i].ContentFileName + "'/></a> </div>" +
+                                    "<p>" + data[i].ThumbName + "</p>" +
                               "</div>";
                 dataCarosello.addItem(htmlStr);
             }
